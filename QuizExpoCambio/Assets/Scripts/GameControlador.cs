5c828a61e36b4977f88fcbab3d50e25b234255e4
Jean Nunes <jeannobreganunes@hotmail.com>
2019-10-22 16:45:34 -0300

    Iniciando pontuacao

---
+++ QuizExpoCambio/Assets/Scripts/GameControlador.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameControlador : MonoBehaviour
+{
+    public TempoControlador scriptTempo;
+    private int contadorPergunta;
+    public string NomeFase;
+    public int pontos, pontosAntigos;
+    public Text txtPontos;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txtPontos.text = pontos.ToString();
+
+
+        if (scriptTempo.segundos <= 0)
+        {
+            contadorPergunta += 1;
+            SceneManager.LoadScene(NomeFase);
+        }
+    }
+
+    public void RespostaCorreta()
+    {
+        pontos += 1;       
+        contadorPergunta += 1;
+        SceneManager.LoadScene(NomeFase);
+        Debug.Log(pontos);
+    }
+
+    public void RespostaErrada()
+    {
+        contadorPergunta += 1;
+        SceneManager.LoadScene(NomeFase);
+        Debug.Log(pontos);
+    }
+
+    public void GuardarPontos()
+    {
+       // PontosFinaisTXT.text = Pontuacao.Pontos.ToString();
+    }
+
+    public void AtualziarPontos()
+    {
+
+    }
+}
---
+++ QuizExpoCambio/Assets/Scripts/TempoControlador.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TempoControlador : MonoBehaviour
+{
+    public float segundos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        segundos = 30f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        segundos -= Time.deltaTime;
+
+        var sliderTempo = transform.GetChild(0).GetComponentInChildren<Slider>();
+        sliderTempo.value = segundos;
+    }
+}
