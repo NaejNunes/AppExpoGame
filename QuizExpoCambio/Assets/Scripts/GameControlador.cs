23db0f984706b74b644b6bc13b42e1169e9042a6
Jean Nunes <jeannobreganunes@hotmail.com>
2019-10-23 13:43:54 -0300

    Projeto prototipado falta apenas sons

--- QuizExpoCambio/Assets/Scripts/GameControlador.cs
+++ QuizExpoCambio/Assets/Scripts/GameControlador.cs
@@ -2,58 +2,224 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class GameControlador : MonoBehaviour
 {
     public TempoControlador scriptTempo;
-    private int contadorPergunta;
-    public string NomeFase;
-    public int pontos, pontosAntigos;
-    public Text txtPontos;
+    public GameObject[] perguntas;
+    private int pontos, contadorPergunta;
+    public Text descricaoPontosTxt, pontosTxt;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        contadorPergunta = 1;
     }
 
     // Update is called once per frame
     void Update()
     {
-        txtPontos.text = pontos.ToString();
-
-
-        if (scriptTempo.segundos <= 0)
+        //TROCA DE FASE CASO O TEMPO ACABE
+        if (scriptTempo.segundos < 0)
         {
-            contadorPergunta += 1;
-            SceneManager.LoadScene(NomeFase);
+            TrocaPerguntas();
+            scriptTempo.segundos = 30;
+        }    
+    }
+
+    //MÉTODO QUE ATRIBUIMOS AO BOTÃO COM A RESPOSTA CORRETA
+    public void RespostaCorreta()
+    {
+        pontos += 1;
+        TrocaPerguntas();
+    }
+
+    //MÉTODO QUE ATRIBUIMOS AO BOTÃO COM A RESPOSTA ERRADA
+    public void RespostaErrada()
+    {
+        TrocaPerguntas();
+    }
+
+    //FINALIZA O JOGO QUANDO AS PERGUNTAS ACABAREM
+    public void FimDeFase()
+    {
+        if (pontos < 3)
+        {
+            descricaoPontosTxt.text = "Você foi um gerreiro, mesmo não sabendo muita coisa chegou até o final. \n Parabéns!";
+            pontosTxt.text = "" + pontos;
+        }
+        else if (pontos > 3 && pontos < 8)
+        {
+            descricaoPontosTxt.text = "Olha só você entende sobre esse assunto. \n Parabéns!";
+            pontosTxt.text = "" + pontos;
+        }
+        else if (pontos > 7 && pontos < 10)
+        {
+            descricaoPontosTxt.text = "Opa! O que é isso? temos um Mestre na arte! \n Parabéns! VOCÊ É DE MAIS!!!";
+            pontosTxt.text = "" + pontos;
+        }
+        else if (pontos == 10)
+        {
+            descricaoPontosTxt.text = "O senhor do NERDS me deixe ser seu discípulo!!!";
+            pontosTxt.text = "" + pontos;
+        }
+    }
+    //MÉTODO QUE TROCA AS PERGUNTAS
+    public void TrocaPerguntas()
+    {
+        contadorPergunta++;
+
+        switch (contadorPergunta)
+        {
+            case 1:
+                perguntas[0].SetActive(true);
+                perguntas[1].SetActive(false);
+                perguntas[2].SetActive(false);
+                perguntas[3].SetActive(false);
+                perguntas[4].SetActive(false);
+                perguntas[5].SetActive(false);
+                perguntas[6].SetActive(false);
+                perguntas[7].SetActive(false);
+                perguntas[8].SetActive(false);
+                perguntas[9].SetActive(false);
+                break;
+
+            case 2:
+                perguntas[0].SetActive(false);
+                perguntas[1].SetActive(true);
+                perguntas[2].SetActive(false);
+                perguntas[3].SetActive(false);
+                perguntas[4].SetActive(false);
+                perguntas[5].SetActive(false);
+                perguntas[6].SetActive(false);
+                perguntas[7].SetActive(false);
+                perguntas[8].SetActive(false);
+                perguntas[9].SetActive(false);
+                break;
+
+            case 3:
+                perguntas[0].SetActive(false);
+                perguntas[1].SetActive(false);
+                perguntas[2].SetActive(true);
+                perguntas[3].SetActive(false);
+                perguntas[4].SetActive(false);
+                perguntas[5].SetActive(false);
+                perguntas[6].SetActive(false);
+                perguntas[7].SetActive(false);
+                perguntas[8].SetActive(false);
+                perguntas[9].SetActive(false);
+                break;
+
+            case 4:
+                perguntas[0].SetActive(false);
+                perguntas[1].SetActive(false);
+                perguntas[2].SetActive(false);
+                perguntas[3].SetActive(true);
+                perguntas[4].SetActive(false);
+                perguntas[5].SetActive(false);
+                perguntas[6].SetActive(false);
+                perguntas[7].SetActive(false);
+                perguntas[8].SetActive(false);
+                perguntas[9].SetActive(false);
+                break;
+
+            case 5:
+                perguntas[0].SetActive(false);
+                perguntas[1].SetActive(false);
+                perguntas[2].SetActive(false);
+                perguntas[3].SetActive(false);
+                perguntas[4].SetActive(true);
+                perguntas[5].SetActive(false);
+                perguntas[6].SetActive(false);
+                perguntas[7].SetActive(false);
+                perguntas[8].SetActive(false);
+                perguntas[9].SetActive(false);
+                break;
+
+            case 6:
+                perguntas[0].SetActive(false);
+                perguntas[1].SetActive(false);
+                perguntas[2].SetActive(false);
+                perguntas[3].SetActive(false);
+                perguntas[4].SetActive(false);
+                perguntas[5].SetActive(true);
+                perguntas[6].SetActive(false);
+                perguntas[7].SetActive(false);
+                perguntas[8].SetActive(false);
+                perguntas[9].SetActive(false);
+                break;
+
+            case 7:
+                perguntas[0].SetActive(false);
+                perguntas[1].SetActive(false);
+                perguntas[2].SetActive(false);
+                perguntas[3].SetActive(false);
+                perguntas[4].SetActive(false);
+                perguntas[5].SetActive(false);
+                perguntas[6].SetActive(true);
+                perguntas[7].SetActive(false);
+                perguntas[8].SetActive(false);
+                perguntas[9].SetActive(false);
+                break;
+
+            case 8:
+                perguntas[0].SetActive(false);
+                perguntas[1].SetActive(false);
+                perguntas[2].SetActive(false);
+                perguntas[3].SetActive(false);
+                perguntas[4].SetActive(false);
+                perguntas[5].SetActive(false);
+                perguntas[6].SetActive(false);
+                perguntas[7].SetActive(true);
+                perguntas[8].SetActive(false);
+                perguntas[9].SetActive(false);
+                break;
+
+            case 9:
+                perguntas[0].SetActive(false);
+                perguntas[1].SetActive(false);
+                perguntas[2].SetActive(false);
+                perguntas[3].SetActive(false);
+                perguntas[4].SetActive(false);
+                perguntas[5].SetActive(false);
+                perguntas[6].SetActive(false);
+                perguntas[7].SetActive(false);
+                perguntas[8].SetActive(true);
+                perguntas[9].SetActive(false);
+                break;
+
+            case 10:
+                perguntas[0].SetActive(false);
+                perguntas[1].SetActive(false);
+                perguntas[2].SetActive(false);
+                perguntas[3].SetActive(false);
+                perguntas[4].SetActive(false);
+                perguntas[5].SetActive(false);
+                perguntas[6].SetActive(false);
+                perguntas[7].SetActive(false);
+                perguntas[8].SetActive(false);
+                perguntas[9].SetActive(true);
+                break;
+
+            case 11:
+                perguntas[0].SetActive(false);
+                perguntas[1].SetActive(false);
+                perguntas[2].SetActive(false);
+                perguntas[3].SetActive(false);
+                perguntas[4].SetActive(false);
+                perguntas[5].SetActive(false);
+                perguntas[6].SetActive(false);
+                perguntas[7].SetActive(false);
+                perguntas[8].SetActive(false);
+                perguntas[9].SetActive(false);
+
+                perguntas[10].SetActive(true);
+                FimDeFase();
+                Time.timeScale = 0;
+
+                break;
         }
     }
 
-    public void RespostaCorreta()
-    {
-        pontos += 1;       
-        contadorPergunta += 1;
-        SceneManager.LoadScene(NomeFase);
-        Debug.Log(pontos);
-    }
-
-    public void RespostaErrada()
-    {
-        contadorPergunta += 1;
-        SceneManager.LoadScene(NomeFase);
-        Debug.Log(pontos);
-    }
-
-    public void GuardarPontos()
-    {
-       // PontosFinaisTXT.text = Pontuacao.Pontos.ToString();
-    }
-
-    public void AtualziarPontos()
-    {
-
-    }
 }
